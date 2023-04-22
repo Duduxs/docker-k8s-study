949f19c8d31255ff7f31bcafb33923b5d7f302c4
Eduardo José <duduxss3@gmail.com>
2023-04-22 19:11:21 -0300

    putting docker multistage build on init and destroy shell scripts

--- fullcycle/java-with-quarkus/destroy.sh
+++ fullcycle/java-with-quarkus/destroy.sh
@@ -1,1 +1,7 @@
 docker rm -f quarkus-in-alpine && docker rmi duduxs/quarkus-in-alpine:v1
+
+echo "Now removing multistage build quarkus container and images"
+
+docker rm --force quarkus-in-alpine-multistage-build && docker rmi duduxs/quarkus-in-alpine-multistage-build:v1
+
+
--- fullcycle/java-with-quarkus/init.sh
+++ fullcycle/java-with-quarkus/init.sh
@@ -1,1 +1,9 @@
 docker build -t duduxs/quarkus-in-alpine:v1 . && docker run --name quarkus-in-alpine -d -p 8099:8500 -e PORT=8500 --network bridge --mount type=bind,source="$(pwd)/quarkus-greeting-resource",destination=/home/quarkus-in-container/src/main/java/com/edudev duduxs/quarkus-in-alpine:v1
+
+echo "Now bulding multistage quarkus image"
+
+docker build -t duduxs/quarkus-in-alpine-multistage-build:v1 . -f ./Dockerfile-with-multistage-building.prd && docker run --name quarkus-in-alpine-multistage-build -d -p 8098:8500 -e PORT=8500 --mount type=bind,source="$(pwd)/quarkus-greeting-resource",destination=/home/quarkus-in-container/src/main/java/com/edudev duduxs/quarkus-in-alpine-multistage-build:v1
+
+
+
+
