196860245ffb4ed85d0df3da13b7c7054f45fd61
Eduardo José <duduxss3@gmail.com>
2023-04-22 21:39:26 -0300

    Making node with express connect with mysql

--- fullcycle/node/index.js
+++ fullcycle/node/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+	host: 'mysql',
+	user: 'root',
+	password: 'root',
+	database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people (name) values ('Eduardo J')`
+connection.query(sql)
 
 app.get('/', (req, res) => {
  res.send('<h1>Eduardo J</h1>')
+	connection.query(`INSERT INTO people (name) VALUES ('Eduardo feito via curl')`)
+	connection.end()
 } )
 
 
