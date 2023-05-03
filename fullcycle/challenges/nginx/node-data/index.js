c13f791e32e9c4183b091780ed012552a3a29d30
Eduardo José <duduxss3@gmail.com>
2023-05-03 20:41:49 -0300

    Making a lot of changes

--- fullcycle/challenges/nginx/node-data/index.js
+++ fullcycle/challenges/nginx/node-data/index.js
@@ -8,17 +8,27 @@ const config = {
     user: 'root',
     password: 'eduardo',
     database: 'node',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 }
 
-const connection = database.createConnection(config)
+const connection = database.createPool(config).promise();
 
-app.get("/", (req,res) => {
-res.send("<h1>Full Cycle Rocks</h1>")
-connection.query(`INSERT INTO people (name) values ('Eduardo J')`)
-connection.end()
+app.get("/", async (req,res) => {
+
+await connection.query(`INSERT INTO people (name) values ('Eduardo J')`);
+
+var data = await getData();
+
+res.send("<h1>Full Cycle Rocks!</h1>" + data);
+
 });
 
-app.listen(3000, () => {
-console.log("Listening on port 3000") 
-});
+app.listen(3000, () => { console.log("Listening on port 3000!")  });
+
+async function getData() {
+var data = await connection.query(`SELECT name from people`);
+return JSON.stringify(data[0]);
+}
 
